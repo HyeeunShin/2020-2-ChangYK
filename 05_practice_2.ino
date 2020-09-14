6853026e39fcd10865c06b670038209a3b73e20c
Hyeeun Shin <as221323@kookmin.ac.kr>
2020-09-14 16:25:36 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,47 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(7 , OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+ toggle = 0;
+ digitalWrite(7, toggle);
+  
+}
+
+void loop() {
+  delay(1000);
+  
+  toggle = 1;
+  digitalWrite(7, toggle);
+  delay(1000);
+  
+  for (int i = 0; i<11; i ++) {
+    toggle =toggle_state(toggle); //toggle LED value.
+    digitalWrite(7 , toggle); // update LED status.
+    delay(100);// wait for 250 milliseconds
+  }
+
+  toggle = toggle_state(toggle);
+  while (1) {
+    digitalWrite(7, toggle);
+  }
+}
+
+
+
+int toggle_state(int toggle) {
+
+  if (toggle == 0)
+  {
+    toggle = 1;
+  }
+  else
+  {
+    toggle = 0;
+   }
+  return toggle;
+}
